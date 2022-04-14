19bc3c8d782ba98da18f741c6ef684db4a9ce056
zhan-yao <3119954407@qq.com>
2022-04-14 22:50:44 +0800

    初步开发注册页面

--- src/main/java/com/nowcoder/community/service/UserService.java
+++ src/main/java/com/nowcoder/community/service/UserService.java
@@ -2,8 +2,16 @@ package com.nowcoder.community.service;
 
 import com.nowcoder.community.dao.UserMapper;
 import com.nowcoder.community.entity.User;
+import com.nowcoder.community.util.MailClient;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class UserService {
@@ -11,8 +19,44 @@ public class UserService {
     @Autowired
     private UserMapper userMapper;
 
+    @Autowired
+    private MailClient mailClient;
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @Value("community.path.domain")
+    private String domain;
+
+    @Value("server.servlet.context-path")
+    private String contextPath;
+
     public User findUserById(int id) {
         return userMapper.selectById(id);
     }
 
+    public Map<String,Object> register(User user){
+        Map<String,Object> map=new HashMap<>();
+        if(user==null)
+            throw new IllegalArgumentException("参数不能为空！！！！");
+        if(StringUtils.isBlank(user.getUsername())){
+            map.put("usernameMsg","账号不能为空");
+            return map;
+        }
+        if(StringUtils.isBlank(user.getPassword())){
+            map.put("passwordMsg","密码不能为空");
+            return map;
+        }
+        if(StringUtils.isBlank(user.getEmail())){
+            map.put("emailMsg","邮箱不能为空");
+            return map;
+        }
+        //验证账号
+        User u=userMapper.selectByName(user.getUsername());
+        if(u!=null){
+            map.put("usernameMsg","该账号已存在");
+            return map;
+        }
+        return map;
+    }
 }
