6eb93ff8897a4aef62d555372f1df2e678ae0763
zhan-yao <3119954407@qq.com>
2022-04-15 21:04:16 +0800

    初步开发注册功能

---
+++ src/test/java/com/nowcoder/community/MailTest.java
@@ -0,0 +1,22 @@
+package com.nowcoder.community;
+
+import com.nowcoder.community.util.MailClient;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class MailTest {
+    @Autowired
+    private MailClient mailClient;
+
+    @Test
+    public void mainTest(){
+        mailClient.sendMail("3119954407@qq.com","测试","<h1>nihsishui</h1>");
+    }
+}
