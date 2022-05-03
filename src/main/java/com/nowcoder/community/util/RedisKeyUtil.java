e83a3ec88e2a68f2a17007878ebe32cbbbb2fde0
zhan-yao <3119954407@qq.com>
2022-05-03 22:41:43 +0800

    点赞功能

---
+++ src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
@@ -0,0 +1,21 @@
+package com.nowcoder.community.util;
+
+public class RedisKeyUtil {
+
+    private static final String SPLIT = ":";
+    private static final String PREFIX_ENTITY_LIKE = "like:entity";
+    private static final String PREFIX_USER_LIKE = "like:user";
+
+    // 某个实体的赞.使用set而不是int，主要为了方便后面查看点赞人的信息
+    // like:entity:entityType:entityId -> set(userId)
+    public static String getEntityLikeKey(int entityType, int entityId) {
+        return PREFIX_ENTITY_LIKE + SPLIT + entityType + SPLIT + entityId;
+    }
+
+    // 某个用户的赞
+    // like:user:userId -> int
+    public static String getUserLikeKey(int userId) {
+        return PREFIX_USER_LIKE + SPLIT + userId;
+    }
+
+}
